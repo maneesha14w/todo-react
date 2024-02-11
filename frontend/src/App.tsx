b84a48f1ad72da2e81c58cefc3decaa0f576ac30
Maneesha <maneesha14w@gmail.com>
2024-02-11 15:23:21 +0530

    feat: added markup and basic styling.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,33 @@
 import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
-import "./App.css"
+import "./styles.css"
 
 function App() {
-	return <></>
+	function onSubmitTodo(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault()
+	}
+	return (
+		<>
+			<form onSubmit={onSubmitTodo} method="get">
+				<div className="form">
+					<label htmlFor="newTodo">New Todo:</label>
+					<input type="text" id="newTodo" />
+					<button className="btn" type="submit">
+						Add
+					</button>
+				</div>
+			</form>
+			<h1>Todo List</h1>
+			<ul>
+				<li>
+					<div className="titleAndStatus">
+						<input type="checkbox" name="todo" id="" />
+						<p>Todo</p>
+					</div>
+					<button className="btn btn-danger">Delete</button>
+				</li>
+			</ul>
+		</>
+	)
 }
 
 export default App
