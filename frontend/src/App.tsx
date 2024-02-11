088611ec25bebf6bb4dd2a852f35066551b233a7
Maneesha <maneesha14w@gmail.com>
2024-02-11 15:45:02 +0530

    fix: fixed input to reflect current value and save in state.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,27 @@ import { useState } from "react"
 import "./styles.css"
 
 function App() {
+	const [newTodo, setNewtodo] = useState("")
+	const [todo, setTodo] = useState("")
+
 	function onSubmitTodo(e: React.FormEvent<HTMLFormElement>) {
+		// Prevents the default form submission behavior of refreshing
 		e.preventDefault()
+		console.log(newTodo)
 	}
+
 	return (
 		<>
 			<form onSubmit={onSubmitTodo} method="get">
 				<div className="form">
 					<label htmlFor="newTodo">New Todo:</label>
-					<input type="text" id="newTodo" />
+					<input
+						type="text"
+						value={newTodo}
+						onChange={(e) => setNewtodo(e.target.value)}
+						id="newTodo"
+						autoComplete="off"
+					/>
 					<button className="btn" type="submit">
 						Add
 					</button>
