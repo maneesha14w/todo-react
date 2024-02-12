2c97807e75ced120bd6ad36efcc104ced1d8044b
Maneesha <maneesha14w@gmail.com>
2024-02-12 13:52:12 +0530

    fix: fixed delete all and added icons and additional styling.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -54,9 +54,7 @@ function App() {
 		// const copyOfTodos = todos.slice()
 		// filter out todos if id is not equal to passed id
 		setTodos((todos) => {
-			return todos.filter((todo) => {
-				todo.id != id
-			})
+			return todos.filter((todo) => todo.id != id)
 		})
 	}
 
@@ -82,8 +80,27 @@ function App() {
 				{todos.map((todo: Todo) => {
 					return (
 						<div key={todo.id}>
-							<li key={todo.id}>
+							<li className="li" key={todo.id}>
 								<div className="titleAndStatus">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="24"
+										height="24"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										stroke="currentColor"
+										fill="none"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+									>
+										<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+										<path d="M9 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+										<path d="M9 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+										<path d="M9 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+										<path d="M15 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+										<path d="M15 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+										<path d="M15 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+									</svg>
 									<input
 										type="checkbox"
 										checked={todo.isComplete}
@@ -99,14 +116,45 @@ function App() {
 										className="btn btn-edit"
 										onClick={() => editTodo(todo.id, newTodo)}
 									>
-										Edit
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											width="24"
+											height="24"
+											viewBox="0 0 24 24"
+											stroke-width="1.5"
+											stroke="currentColor"
+											fill="none"
+											stroke-linecap="round"
+											stroke-linejoin="round"
+										>
+											<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+											<path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" />
+											<path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" />
+											<path d="M16 5l3 3" />
+										</svg>
 									</button>
 									<button
 										onClick={() => deleteTodo(todo.id)}
 										className="btn btn-danger"
-										// onClick={deleteTodo(todo.id)}
 									>
-										Delete
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											width="24"
+											height="24"
+											viewBox="0 0 24 24"
+											stroke-width="1.5"
+											stroke="currentColor"
+											fill="none"
+											stroke-linecap="round"
+											stroke-linejoin="round"
+										>
+											<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+											<path d="M4 7l16 0" />
+											<path d="M10 11l0 6" />
+											<path d="M14 11l0 6" />
+											<path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+											<path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+										</svg>
 									</button>
 								</div>
 							</li>
